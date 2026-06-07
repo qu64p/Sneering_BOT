a654ffee5209e3aa8598fd2370c1d6c128ef06fb
tyaha-n <satoaki.0505.koyama@gmail.com>
2026-06-07 19:14:25 +0900

    Update main.go

--- main.go
+++ main.go
@@ -15,39 +15,37 @@ import (
 func main() {
 	token := os.Getenv("DISCORD_TOKEN")
 	if token == "" {
-		log.Fatal("DISCORD_TOKEN が設定されていません")
+		log.Fatal("DISCORD_TOKENが設定されていません")
 	}
 
-	// Supabase 初期化
 	if err := db.Init(); err != nil {
-		log.Fatalf("Supabase 初期化失敗: %v", err)
+		log.Fatalf("Supabase初期化失敗: %v", err)
 	}
 
-	// Discord セッション作成
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
-		log.Fatalf("Discord セッション作成失敗: %v", err)
+		log.Fatalf("Discordセッション作成失敗: %v", err)
 	}
 
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	dg.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuilds
 
 	h := handler.New()
 
-	// イベントハンドラ登録
 	dg.AddHandler(h.OnMessage)
 	dg.AddHandler(h.OnInteraction)
 	dg.AddHandler(h.OnReady)
+	dg.AddHandler(h.OnGuildDelete)
 
 	if err := dg.Open(); err != nil {
-		log.Fatalf("Discord 接続失敗: %v", err)
+		log.Fatalf("Discord接続失敗: %v", err)
 	}
 	defer dg.Close()
 
-	log.Println("Bot が起動しました。Ctrl+C で終了します。")
+	log.Println("Botが起動しました。Ctrl+Cで終了します。")
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
 	<-sc
 
-	log.Println("Bot を終了します。")
+	log.Println("Botを終了します。")
 }
